afb595b1f0b2a590f43ffd60f5e20104b3a2c4b5
vyk <vyncem@gmail.com>
2025-06-25 13:28:48 +0300

    add paystack pop

---
+++ app.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+import PaystackPop from '@paystack/inline-js'
+
+var args = process.argv.slice(2);
+
+console.log(index + ': ' + val);
+const popup = new PaystackPop()
+popup.resumeTransaction("np00q2xw1l")
